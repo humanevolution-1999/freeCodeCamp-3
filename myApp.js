c972dd78104731c49f5afac2cf2810c38eaf40fa
Rana Madhvendra <rana.madhvendra@gmail.com>
2024-11-14 17:32:39 +0530

    Updated file

--- myApp.js
+++ myApp.js
@@ -1,4 +1,9 @@
 require('dotenv').config();
+//console.log(process.env.MONGO_URI);
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 
 let Person;
